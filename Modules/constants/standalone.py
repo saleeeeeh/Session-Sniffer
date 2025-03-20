811ac397d96064d6241365835ff4fd6a949b4d86
IB_U_Z_Z_A_R_Dl <BUZZARDGTA@protonmail.com>
2025-03-20 11:29:56 +0100

    Update standalone.py

--- Modules/constants/standalone.py
+++ Modules/constants/standalone.py
@@ -1,5 +1,5 @@
 TITLE = "Session Sniffer"
-VERSION = "v1.3.6 - 20/03/2025 (11:28)"
+VERSION = "v1.3.7 - 20/03/2025 (11:29)"
 
 GITHUB_REPO_URL = "https://github.com/BUZZARDGTA/Session-Sniffer"
 GITHUB_RELEASES_URL = "https://github.com/BUZZARDGTA/Session-Sniffer/releases"
